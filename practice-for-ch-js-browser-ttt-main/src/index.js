6656d8c9ca008de7b16a8579b67f278b81cba4de
Ethanjonm <ethanjonmercado@gmail.com>
2023-05-02 16:18:50 -0400

    fix grid

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -6,7 +6,5 @@ document.addEventListener("DOMContentLoaded", () => {
   const game = new Game();
   const el = document.querySelector(".ttt");
   new View(game, el);
-  const v = new View(game, el);
-  window.v = v
-  v.setupBoard();
+
 });
